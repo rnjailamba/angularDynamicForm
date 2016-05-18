dc64aaf9cc4d719b5dc36577b598fa3be557c26c
rnjai <rnjailamba12@gmail.com>
2016-05-19 05:11:58 +0530

    Added schema form with basic example

---
+++ app/src/users/FormController.js
@@ -0,0 +1,25 @@
+angular.module('starterApp')
+       .controller('FormController', FormController);
+
+function FormController($scope) {
+  $scope.schema = {
+    type: "object",
+    properties: {
+      name: { type: "string", minLength: 2, title: "Name", description: "Name or alias" },
+      title: {
+        type: "string",
+        enum: ['dr','jr','sir','mrs','mr','NaN','dj']
+      }
+    }
+  };
+
+  $scope.form = [
+    "*",
+    {
+      type: "submit",
+      title: "Save"
+    }
+  ];
+
+  $scope.model = {};
+}
