530530b8aaf47d47ef6306c4e3e1676992f08940
rnjai <rnjailamba12@gmail.com>
2016-05-19 22:27:43 +0530

    Added bootstrap grid form and know how to add classes now

--- app/src/users/FormController4.js
+++ app/src/users/FormController4.js
@@ -1,26 +1,74 @@
 angular.module('starterApp')
        .controller('FormController4', FormController4);
 function FormController4($scope) {
-  $scope.schema = {
-    type: "object",
-    properties: {
-      name: { type: "string", minLength: 2, title: "Name", description: "Name or alias" },
-      title: {
-        type: "string",
-        enum: ['dr','jr','sir','mrs','mr','NaN','dj']
-      }
-    }
-  };
+  $scope.schema =
+                  {
+                    "type": "object",
+                    "title": "Comment",
+                    "properties": {
+                      "name": {
+                        "title": "Name",
+                        "type": "string"
+                      },
+                      "email": {
+                        "title": "Email",
+                        "type": "string",
+                        "pattern": "^\\S+@\\S+$",
+                        "description": "Email will be used for evil."
+                      },
+                      "comment": {
+                        "title": "Comment",
+                        "type": "string",
+                        "maxLength": 20,
+                        "validationMessage": "Don't be greedy!"
+                      }
+                    },
+                    "required": [
+                      "name",
+                      "email",
+                      "comment"
+                    ]
+                  };
 
-  $scope.form = [
-    "*",
-    {
-      type: "submit",
-      title: "Save"
-    }
-  ];
+  $scope.form =
+      [
+        {
+          "type": "help",
+          "helpvalue": "<div class=\"alert alert-info\">Grid it up with bootstrap</div>"
+        },
+        {
+          "type": "section",
+          "htmlClass": "row",
+          "items": [
+            {
+              "type": "section",
+              "htmlClass": "col-xs-6",
+              "items": [
+                "name"
+              ]
+            },
+            {
+              "type": "section",
+              "htmlClass": "col-xs-6",
+              "items": [
+                "email"
+              ]
+            }
+          ]
+        },
+        {
+          "key": "comment",
+          "type": "textarea",
+          "placeholder": "Make a comment"
+        },
+        {
+          "type": "submit",
+          "style": "btn-info",
+          "title": "OK"
+        }
+      ]  ;
 
-  $scope.model = {};
+  $scope.model4 = {};
 
   $scope.onSubmit = function(form) {
     // First we broadcast an event so all fields validate themselves
